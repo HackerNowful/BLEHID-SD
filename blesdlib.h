fd208619fa28736b6074d5ad4857a6b7600f8804
Hacker Nowful <124292739+HackerNowful@users.noreply.github.com>
2024-02-09 00:06:38 +0530

    Add files via upload

---
+++ blesdlib.h
@@ -0,0 +1,19 @@
+
+#ifndef blesdlib_h
+#define blesdlib_h
+
+#include <SPI.h>
+#include <SD.h>
+#include <string.h>
+#include <BleKeyboard.h>
+
+extern BleKeyboard bleKeyboard;
+extern File myFile;
+extern boolean first;
+
+void Line(String l);
+void Press(String b);
+
+void attack();
+
+#endif
